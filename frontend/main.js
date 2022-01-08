43cb8ee884b680b1651ffe7b1b5afa77b002dd1b
manchen <sleep1700505@gmail.com>
2022-01-09 01:01:49 +0800

    fix frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,17 @@
-isReady = true;
-isStart = false;
-isOver = false;
+var isReady = true;
+var isStart = false;
+var isOver = false;
+
+var gameTitle = document.querySelector(".game-title");
+var description = document.querySelector(".description");
+var choiceList = document.querySelector(".choice-list");
+var choiceA = document.querySelector(".button-left");
+var choiceB = document.querySelector(".button-right");
+var startButton = document.querySelector(".start");
+
+choiceA.addEventListener("click", handleChoice);
+choiceB.addEventListener("click", handleChoice);
+startButton.addEventListener("click", startGame);
 
 window.onload = function(){
     setInterval("toggleSound()",100);
@@ -13,4 +24,18 @@ function toggleSound() {
            music.paused=false;
            music.play(); //沒有就播放 
        }    
+}
+
+function handleChoice(obj) {
+
+}
+
+function startGame(obj) {
+    isReady = false;
+    isStart = true;
+    gameTitle.style.display = "none";
+    startButton.style.display = "none";
+    description.style.display = "block";
+    choiceList.style.display = "block";
+    choiceList.style.visibility = "hidden";
 }

edf9c5731b0502dcd7aae76a32442f570f85f319
manchen <sleep1700505@gmail.com>
2022-01-09 00:34:20 +0800

    add frontend

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+isReady = true;
+isStart = false;
+isOver = false;
+
+window.onload = function(){
+    setInterval("toggleSound()",100);
+}
+
+function toggleSound() {
+       var music = document.getElementById("bgm");//獲取ID  
+           
+       if (music.paused) { //判讀是否播放  
+           music.paused=false;
+           music.play(); //沒有就播放 
+       }    
+}
